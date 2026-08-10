e54c48e1ad4e609373b9302a262bd3b6a7b28eaf
DacPhong2003 <dacphong2003@users.noreply.github.com>
2026-08-10 12:55:02 +0700

    polish: empty state dep hon cho trang Thong bao va Giao dich

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chitieu-v7';
+const CACHE = 'chitieu-v8';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
