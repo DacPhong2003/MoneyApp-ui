6fbd296c459eec0750590e7f082a20c07ddb7d16
DacPhong2003 <dacphong2003@users.noreply.github.com>
2026-08-10 12:58:25 +0700

    fix: font-size input/textarea/select len 16px de iOS khong tu zoom khi focus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chitieu-v9';
+const CACHE = 'chitieu-v10';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
